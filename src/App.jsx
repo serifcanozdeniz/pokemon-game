d0c5f24540fa55768204982354d9fdfa9fc71dc2
serifcanozdeniz <ozdenizserifcan96@gmail.com>
2024-01-28 22:34:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./Pokecard.css";
+import PokeGame from "./PokeGame";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Go Card Game</h1>
+      <PokeGame />
+    </div>
+  );
+}
+
+export default App;
